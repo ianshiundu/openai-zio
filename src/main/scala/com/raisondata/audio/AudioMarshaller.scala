1d9b017148629bb942db8c43801c58430b5b04f1
Ian Shiundu <ian.shiundu@gmail.com>
2023-04-01 22:22:04 +0300

    Project Init

---
+++ src/main/scala/com/raisondata/audio/AudioMarshaller.scala
@@ -0,0 +1,12 @@
+package com.raisondata.audio
+
+import io.circe._
+import io.circe.generic.semiauto._
+
+trait AudioMarshaller {
+  case class TranscribeResponse(text: String)
+
+  object TranscribeResponse {
+    implicit val decoder: Decoder[TranscribeResponse] = deriveDecoder[TranscribeResponse]
+  }
+}
