444474c6f40a4e9826df9e7ea1ff669895f10aa5
Ian Shiundu <ian.shiundu@gmail.com>
2023-04-01 22:51:04 +0300

    Code cleanup

--- src/main/scala/com/raisondata/audio/Transcription.scala
+++ src/main/scala/com/raisondata/audio/Transcription.scala
@@ -9,29 +9,18 @@ import zio._
 
 import java.io.File
 
-trait Transcription {
-  def transcribe(
-                  filePath: String,
-                  prompt: Option[String],
-                  responseFormat: Option[String], // Enum maybe?
-                  temperature: Option[Int],
-                  language: Option[String], //ISO-639-1 format
-                  model: String = "whisper-1", //Enum maybe?
-                ): Task[Response[Either[ResponseException[String, circe.Error], Transcription.TranscribeResponse]]]
-}
-
 object Transcription extends SttpConfig with AudioMarshaller {
   override def usage: String = "transcriptions"
   override def domain: String = "audio"
 
   def transcribe(
       filePath: String,
-      model: String = "whisper-1", //Enum maybe?
+      model: String = "whisper-1", // Enum maybe?
       prompt: Option[String] = None,
       responseFormat: Option[String] = None, // Enum maybe?
       temperature: Option[Int] = None,
-      language: Option[String] = None, //ISO-639-1 format
-                ): ZIO[SttpClient, Throwable, TranscribeResponse] = {
+      language: Option[String] = None // ISO-639-1 format
+  ): ZIO[SttpClient, Throwable, TranscribeResponse] = {
 
     val audioFile = new File(filePath)
 
@@ -40,14 +29,16 @@ object Transcription extends SttpConfig with AudioMarshaller {
         .post(uri)
         .header("Authorization", s"Bearer $openaiAPIKey")
         .contentType(MultipartFormData)
-        .multipartBody(Seq(
-          multipartFile("file", audioFile),
-          multipart("model", model),
-          multipart("prompt", prompt),
-          multipart("response_format", responseFormat.getOrElse("json")),
-          multipart("temperature", temperature.getOrElse(0)),
-          multipart("language", language.getOrElse("en"))
-        ))
+        .multipartBody(
+          Seq(
+            multipartFile("file", audioFile),
+            multipart("model", model),
+            multipart("prompt", prompt),
+            multipart("response_format", responseFormat.getOrElse("json")),
+            multipart("temperature", temperature.getOrElse(0)),
+            multipart("language", language.getOrElse("en"))
+          )
+        )
         .readTimeout(5.minute.asScala)
         .response(asJson[TranscribeResponse])
 
