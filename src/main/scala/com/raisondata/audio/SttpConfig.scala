13ea0b45f84882c92acf77635aa9467bd0db108d
Ian Shiundu <ian.shiundu@gmail.com>
2023-04-01 22:41:14 +0300

    Configuration for OpenAI Client

--- src/main/scala/com/raisondata/audio/SttpConfig.scala
+++ src/main/scala/com/raisondata/audio/SttpConfig.scala
@@ -1,8 +1,16 @@
 package com.raisondata.audio
 
+import sttp.client4.UriContext
+
 trait SttpConfig {
-//  def usage: String
-//  def domain: String
+  def usage: String
+  def domain: String
+
+  private val baseURL = "https://api.openai.com"
+
+  private val version = "v1"
+
+  val uri =  uri"$baseURL/$version/$domain/$usage"
 
   val openaiAPIKey = ""
 }
