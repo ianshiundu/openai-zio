9aa5afad16316c37e63f7e34eac308be0f3aa30f
Ian Shiundu <ian.shiundu@gmail.com>
2023-04-11 18:40:56 +0300

    Use Role ADT in Message role field

--- src/main/scala/com/raisondata/openai/Role.scala
+++ src/main/scala/com/raisondata/openai/Role.scala
@@ -1,5 +1,9 @@
 package com.raisondata.openai
 
+import io.circe._
+import io.circe.generic.semiauto._
+import io.circe.syntax._
+
 object Role extends Enumeration {
   type Role = Value
 
@@ -7,4 +11,9 @@ object Role extends Enumeration {
 
   def parse(role: Role): String = role.toString
 
+  implicit val encoder: Encoder[Role] =
+    Encoder.encodeEnumeration(Role)
+
+  implicit val decoder: Decoder[Role] =
+    Decoder.decodeEnumeration(Role)
 }
