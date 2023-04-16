eaf26cca15f83d0f6092f782c2fa768788e99635
Ian Shiundu <ian.shiundu@gmail.com>
2023-04-16 17:51:19 +0300

    make function testable

--- src/main/scala/com/raisondata/openai/api/chat/CreateChat.scala
+++ src/main/scala/com/raisondata/openai/api/chat/CreateChat.scala
@@ -1,12 +1,15 @@
 package com.raisondata.openai.api.chat
 
+import com.raisondata.openai.JsonMarshaller._
 import com.raisondata.openai.Model.Model
-import com.raisondata.openai.{Model, SttpConfig}
+import com.raisondata.openai.{JsonMarshaller, Model, SttpConfig}
+import io.circe
+import sttp.capabilities.zio.ZioStreams
 import sttp.client4.circe._
-import sttp.client4.httpclient.zio._
+import sttp.client4.{ResponseException, WebSocketStreamBackend}
 import zio._
 
-object CreateChat extends SttpConfig with ChatMarshaller {
+object CreateChat extends SttpConfig {
   override def domain: String = "chat"
 
   override def usage: String = "completions"
@@ -24,38 +27,39 @@ object CreateChat extends SttpConfig with ChatMarshaller {
       logit_bias: Map[String, Double],
       max_tokens: Option[Int],
       stop: Option[Array[String]]
-  )(openaiAPIKey: String): ZIO[Any, Throwable, CreateChat.ChatResponse] =
-    HttpClientZioBackend().flatMap { backend =>
-      val requestBody = ChatRequest(
-        Model.parse(model),
-        messages,
-        user,
-        max_tokens,
-        temperature,
-        top_p,
-        n,
-        stream,
-        presence_penalty,
-        frequency_penalty,
-        logit_bias,
-        stop
-      )
+  )(openaiAPIKey: String)(implicit
+      backend: WebSocketStreamBackend[Task, ZioStreams]
+  ): ZIO[Any, Throwable, ChatResponse] = {
+    val requestBody = ChatRequest(
+      Model.parse(model),
+      messages,
+      user,
+      max_tokens,
+      temperature,
+      top_p,
+      n,
+      stream,
+      presence_penalty,
+      frequency_penalty,
+      logit_bias,
+      stop
+    )
 
-      val request =
-        requestWithJsonBody(requestBody, asJson[ChatResponse])(openaiAPIKey)
-      val response = getResponse(request)(backend)
+    val request =
+      requestWithJsonBody(requestBody, asJson[ChatResponse])(openaiAPIKey)
+    val response = getResponse(request)(backend)
 
-      response.flatMap(_.body match {
-        case Left(error) =>
-          for {
-            _ <- ZIO.logError(
-              s"An error occurred while making a request $error"
-            )
-          } yield throw new RuntimeException(error)
-        case Right(response) =>
-          for {
-            _ <- ZIO.logInfo("Chat reply was successfully returned!")
-          } yield response
-      })
-    }
+    response.flatMap(_.body match {
+      case Left(error: ResponseException[String, circe.Error]) =>
+        for {
+          _ <- ZIO.logError(
+            s"An error occurred while making a request $error"
+          )
+        } yield throw new RuntimeException(error)
+      case Right(response) =>
+        for {
+          _ <- ZIO.logInfo("Chat reply was successfully returned!")
+        } yield response
+    })
+  }
 }
