4913a0c19497279d6446397e96635628978ab90f
Ian Shiundu <ian.shiundu@gmail.com>
2023-04-16 17:51:06 +0300

    make function testable

--- src/main/scala/com/raisondata/openai/api/audio/Transcription.scala
+++ src/main/scala/com/raisondata/openai/api/audio/Transcription.scala
@@ -4,6 +4,7 @@ import com.raisondata.openai.Language.Language
 import com.raisondata.openai.Model.Model
 import com.raisondata.openai.ResponseFormat.ResponseFormat
 import com.raisondata.openai._
+import sttp.capabilities.zio.ZioStreams
 import sttp.client4._
 import sttp.client4.circe._
 import sttp.client4.httpclient.zio._
@@ -22,46 +23,47 @@ object Transcription extends SttpConfig with AudioMarshaller {
       temperature: Int,
       language: Language,
       prompt: Option[String] = None
-  )(openaiAPIKey: String): ZIO[Any, Throwable, TextResponse] =
-    HttpClientZioBackend().flatMap { backend =>
-      require(
-        temperature >= 0 && temperature <= 1,
-        throw new IllegalArgumentException(
-          "Temperature value must be between ranges 0 and 1."
-        )
+  )(openaiAPIKey: String)(implicit
+      backend: WebSocketStreamBackend[Task, ZioStreams]
+  ): ZIO[Any, Throwable, TextResponse] = {
+    require(
+      temperature >= 0 && temperature <= 1,
+      throw new IllegalArgumentException(
+        "Temperature value must be between ranges 0 and 1."
       )
+    )
 
-      val audioFile = new File(filePath)
+    val audioFile = new File(filePath)
 
-      val body = Seq(
-        multipartFile("file", audioFile),
-        multipart("model", Model.parse(model)),
-        multipart("prompt", prompt),
-        multipart(
-          "response_format",
-          ResponseFormat.parse(responseFormat)
-        ),
-        multipart("temperature", temperature),
-        multipart(
-          "language",
-          Language.parse(language)
-        )
+    val body = Seq(
+      multipartFile("file", audioFile),
+      multipart("model", Model.parse(model)),
+      multipart("prompt", prompt),
+      multipart(
+        "response_format",
+        ResponseFormat.parse(responseFormat)
+      ),
+      multipart("temperature", temperature),
+      multipart(
+        "language",
+        Language.parse(language)
       )
+    )
 
-      val request =
-        requestWithMultipartForm(body, asJson[TextResponse])(openaiAPIKey)
-      val response = getResponse(request)(backend)
+    val request =
+      requestWithMultipartForm(body, asJson[TextResponse])(openaiAPIKey)
+    val response = getResponse(request)(backend)
 
-      response.flatMap(_.body match {
-        case Left(error) =>
-          for {
-            _ <- ZIO.logError(s"An error occurred while making a request $error")
-          } yield throw new RuntimeException(error)
-        case Right(response) =>
-          for {
-            _ <- ZIO.logInfo("Audio was transcribed successfully!")
-          } yield response
-      })
-    }
+    response.flatMap(_.body match {
+      case Left(error) =>
+        for {
+          _ <- ZIO.logError(s"An error occurred while making a request $error")
+        } yield throw new RuntimeException(error)
+      case Right(response) =>
+        for {
+          _ <- ZIO.logInfo("Audio was transcribed successfully!")
+        } yield response
+    })
+  }
 
 }
